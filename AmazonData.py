049bc62febd578be945d6f0e704624286e318f16
hatimSa <hatimsalhi03@gmail.com>
2025-01-10 17:44:48 +0100

    data data

--- AmazonData.py
+++ AmazonData.py
@@ -1,57 +1,66 @@
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
-amazon_url = "https://www.amazon.com/RedThunder-Wireless-Rechargeable-Mechanical-Anti-ghosting/dp/B09BR46F63/ref=sr_1_1_sspa?_encoding=UTF8&content-id=amzn1.sym.3ee8a8b8-12a8-4ba9-9886-109b6d3579a2&dib=eyJ2IjoiMSJ9.QjBFM_c24i1J6qA0sqOIwIIWX3m9tFhYPU-D3vb-8EcI7EljpVOYCRmjEi2pg6QYEMyZ8DwhgMnZSP97rUv8Ezfe8IyQhcDZMCGe118TvBVkCfnUMOqyyQk-EUmA_MY5cW-FdUZnq1bbVsF9qSjIht2FkGERjn7VAtXrr5zKqr1PrDUq-tgaO4w5gnZze3wn8ZnstFO1eJDKTQA82d2i-oAbk1Z2vi58Ich0cykj465ShSKaPz80r0QmyFGpJaUHz2fp-OPUeGo1JPnktMbsDEGoUbRUma1lkZ0UOZv0iPo.y6BeWZwY44ODHRmOei0h4gKy7k75OrF5AI6P1ckmA98&dib_tag=se&keywords=gaming&pd_rd_r=07e18d56-767a-45da-8a41-ed1086364614&pd_rd_w=JqqCY&pd_rd_wg=v6442&pf_rd_p=3ee8a8b8-12a8-4ba9-9886-109b6d3579a2&pf_rd_r=27RHMP5RYYGG0QV0KMRD&qid=1736187946&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1"
+# Configuration du navigateur Selenium
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
 
-amazon_custom_headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
-}
+# Créer une instance du navigateur
+driver = webdriver.Chrome(options=options)
 
-amazon_cookies = {
-    'session-id': '131-1289666-6087523',
-    'ubid-main': '131-9196836-4366522',
-    'x-main': '"MycvhchDeAO4NhUd@xFSgRY5OpNjEub8njb?gUVOzTq8XUwen?PzBxdOrXcBVX@9"',
-}
+amazon_url = "https://www.amazon.com/s?k=gaming&ref=nb_sb_noss"
+driver.get(amazon_url)
 
-response = requests.get(amazon_url, headers=amazon_custom_headers, cookies=amazon_cookies)
-soup = BeautifulSoup(response.text, "html.parser")
-    
-# Extraction du titre
-title_element = soup.select_one("#productTitle")
-if title_element:
-    title = title_element.text.strip()
-    print(f"Amazon Title: {title}")
-else:
-    print("Amazon Product title not found")
-    
-# Extraction de la note
-rating_element = soup.select_one("#acrPopover")
-if rating_element and rating_element.attrs.get("title"):
-    rating_text = rating_element.attrs.get("title")
-    print(f"Amazon Rating: {rating_text}")
-else:
-    print("Amazon Rating not found")
+# Attendre que la page soit complètement chargée (par exemple, attendre un élément spécifique)
+driver.implicitly_wait(10)
 
-# Extraction du prix
-price_element = soup.select_one("span.a-offscreen")
-if price_element:
-    price = price_element.text.strip()
-    print(f"Amazon Price: {price}")
-else:
-    print("Amazon Price not found")
+# Obtenir le contenu HTML de la page
+soup = BeautifulSoup(driver.page_source, "html.parser")
 
-# Extraction de l'image
-image_element = soup.select_one("#landingImage")
-if image_element:
-    image_url = image_element.attrs.get("src")
-    print(f"Amazon Image URL: {image_url}")
-else:
-    print("Amazon Image URL not found")
+# Extraction des produits
+products = soup.find_all("div", class_="sg-col-inner")
 
-# Extraction de la description
-description_element = soup.select_one("#feature-bullets")
-if description_element:
-    description = description_element.text.strip()
-    print(f"Amazon Description: {description}")
-else:
-        print("Amazon Description not found")
+# Set pour suivre les liens déjà extraits
+extracted_links = set()
+
+for product in products:
+    try:
+        title_element = product.find("h2", class_="a-size-base-plus a-spacing-none a-color-base a-text-normal")
+        if title_element:
+            title = title_element.text.strip()
+        else:
+            title = "Product title not found"
+
+        price_element = product.find("span", class_="a-offscreen")
+        if price_element:
+            price = price_element.text.strip()
+        else:
+            price = "Price not found"
+
+        rating_element = product.find("span", class_="a-icon-alt")
+        if rating_element:
+            rating = rating_element.text.strip()
+        else:
+            rating = "Rating not found"
+
+        link_element = product.find("a", class_="a-link-normal s-line-clamp-4 s-link-style a-text-normal")
+        if link_element:
+            product_link = "https://www.amazon.com" + link_element.attrs["href"]
+        else:
+            product_link = "Product link not found"
+
+        # Vérifier si le lien du produit a déjà été extrait
+        if product_link not in extracted_links:
+            extracted_links.add(product_link)
+            print(f"Product Title: {title}")
+            print(f"Price: {price}")
+            print(f"Rating: {rating}")
+            print(f"Product Link: {product_link}")
+            print("-" * 50)
+        else:
+            print("Duplicate product, skipping.")
+
+    except Exception as e:
+        print(f"Error extracting product: {e}")
+
+driver.quit()
--- eBayData.py
+++ eBayData.py
@@ -1,56 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-ebay_url = "https://www.ebay.com/itm/315871566918?_trkparms=amclksrc%3DITM%26aid%3D1110018%26algo%3DHOMESPLICE.COMPLISTINGS%26ao%3D1%26asc%3D264184%26meid%3D8cd57dce6bf34c78917d7a783cf0afbf%26pid%3D101196%26rk%3D2%26rkt%3D12%26sd%3D387743508213%26itm%3D315871566918%26pmt%3D1%26noa%3D0%26pg%3D2332490%26algv%3DCompVIDesktopATF2V6&_trksid=p2332490.c101196.m2219&itmprp=cksum%3A3158715669188cd57dce6bf34c78917d7a783cf0afbf%7Cenc%3AAQAJAAABMLx1mZQ2L9jLv%252BUPTFTna8XcGfl2wNJcn022gtZb5sDiFrTi3gwMo%252Fa--7j5u2kQDPo58ZnaS782LdgqLI9am6jY%252FLT0qkSl5rcxT2rvJwfU%252BP88LLNs7p%252Fy12EYYq1k2CU1vc5GX4zQMEyhHPS8rxUz%252F6msFUnNf9xRDCd7Jhd0x7%252FZfMQx9GP8bL9RkF8%252BJFU4JmgC8v%252FGamZw6wCQs8aylUjbSrxZDrKrVygA67eAfI2YJeviejXcQdhmFj%252BPFsP94jiXzteWDqyBxJ5hJOul4SRN6IAsWnR%252F5UfmCuwP2HjbNjR5s0lq%252Bq%252BcopgnZKonD7W564tHzVHM5StDd6l0mshxWPRIMb2ZmkQLFnCqYGw9h5nqjBiIvgvoYC%252F2RjqxKVNTMdw6EULOKrRPgEk%253D%7Campid%3APL_CLK%7Cclp%3A2332490&itmmeta=01JGYHPRMS7C3DJNRFP519G1BA"
+url = "https://www.ebay.com/b/Video-Games-Consoles/1249/bn_1850232"
 
-ebay_custom_headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
-}
+response = requests.get(url)
 
-#ebay_cookies = {
- #   'session-id': 'your-session-id',
-  #  'user-info': 'some-cookie-value',
-#}
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
 
-response = requests.get(ebay_url, headers=ebay_custom_headers)#, cookies=ebay_cookies)
-soup = BeautifulSoup(response.text, "html.parser")
-    
-# Extraction du titre
-title_element = soup.select_one("#ux-textspans ux-textspans--BOLD")
-if title_element:
-    title = title_element.text.strip()
-    print(f"eBay Title: {title}")
+    products = soup.find_all("section", class_="brw-product-carousel brw-limited-time-deals bltd")
+
+    print(f"{len(products)} produits trouvés!!!")
+
+    for product in products:
+        try:
+            title = product.find("h3", class_="textual-display bsig__title__text")
+            old_price = product.find("span", class_="textual-display bsig__price bsig__price--displayprice")
+            price_promo = product.find("span", class_="textual-display bsig__generic bsig__previousPrice strikethrough")
+            link = product.find("a", class_="brw-product-card__image-link")
+
+            print(f"Titre: {title.text.strip() if title else 'N/A'}")
+            print(f"Prix normal: {old_price.text.strip() if old_price else 'N/A'}")
+            print(f"Prix promo: {price_promo.text.strip() if price_promo else 'N/A'}")
+            print(f"Link: {link}")
+            print("-" * 50)
+
+        except Exception as e:
+            print(f"Error extracting product: {e}")
 else:
-    print("eBay Product title not found")
-    
-# Extraction de la note
-rating_element = soup.select_one("#fdbk-detail-seller-rating__value")
-if rating_element:
-    rating_text = rating_element.text.strip()
-    print(f"eBay Rating: {rating_text}")
-else:
-    print("eBay Rating not found")
-
-# Extraction du prix
-price_element = soup.select_one("#span.x-bin-price")
-if price_element:
-    price = price_element.text.strip()
-    print(f"eBay Price: {price}")
-else:
-    print("eBay Price not found")
-
-# Extraction de l'image
-image_element = soup.select_one("#ux-image-carousel-item image-treatment active  image")
-if image_element:    
-    image_url = image_element.attrs.get("src")
-    print(f"eBay Image URL: {image_url}")
-else:
-    print("eBay Image URL not found")
-
-# Extraction de la description
-description_element = soup.select_one("#d-item-description")
-if description_element:
-    description = description_element.text.strip()
-    print(f"eBay Description: {description}")
-else:
-    print("eBay Description not found")
+    print(f"Erreur lors de la récupération de la page principale. Code : {response.status_code}")
